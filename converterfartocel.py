dfefba83ce324a52dc460b54a8a316c46978b11e
Islam <islamkazproject@gmail.com>
2022-08-02 16:38:20 +0300

    Add configuration in settings.json

--- converterfartocel.py
+++ converterfartocel.py
@@ -1,26 +1,8 @@
-# Вам дана строка состоящая только из цифр. 
-# Вам нужно посчитать сколько нулей ("0") находится в начале строки.
-# Входные данные: Строка, состоящая только из цифр.
-# Выходные данные: Целое число.
-def beginning_zeros(number: str) -> int:
-    # your code here
-    count = 0
-    for letter in number:
-        if letter == '0':
-            count += 1
-        else:
-            break
-    return count
-if __name__ == '__main__':
-    print("Example:")
-    print(beginning_zeros('100'))
-    print(beginning_zeros('123456'))
+fahrenheit = 1
+celsius = 1
+def converter_far_to_cel( fahrenheit: float):
+    celsius = 5/9 * (fahrenheit - 32.0)
+    return celsius
 
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert beginning_zeros('100') == 0
-    assert beginning_zeros('001') == 2
-    assert beginning_zeros('100100') == 0
-    assert beginning_zeros('001001') == 2
-    assert beginning_zeros('012345679') == 1
-    assert beginning_zeros('0000') == 4
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+if __name__ = '__main__':
+    print(converter_far_to_cel(77))
