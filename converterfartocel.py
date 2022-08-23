deee885eb310843d2f52ecb05a4141fb42356cb8
Islam <islamkazproject@gmail.com>
2022-08-23 19:30:17 +0300

    Add func celtofar

--- converterfartocel.py
+++ converterfartocel.py
@@ -1,8 +1,6 @@
-fahrenheit = 1
-celsius = 1
-def converter_far_to_cel( fahrenheit: float):
-    celsius = 5/9 * (fahrenheit - 32.0)
-    return celsius
 
-if __name__ = '__main__':
-    print(converter_far_to_cel(77))
+def cel_to_far(cels):
+    return (cels * 9 / 5) + 32 
+
+if __name__ == "__main__":
+    print (cel_to_far(90))
