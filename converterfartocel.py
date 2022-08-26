aaf4ab70d7484e7c721cfd7bd5ee967142eac01a
Islam <islamkazproject@gmail.com>
2022-08-26 13:09:59 +0300

    New adjustments in file converterfartocel.py

--- converterfartocel.py
+++ converterfartocel.py
@@ -1,9 +1,9 @@
 def cel_to_far(cels: str) -> int or float:
     """Returns converted Celsius to Fahrenheit."""
-    if cels < -273.15:
-        return "Error, the number cannot be lower than absolute zero (-273.15), please try again!"
     if type(cels) not in [int, float]:
         raise TypeError("Celsius must be non-negative real number only")
+    if cels < -273.15:
+        return "Error, the number cannot be lower than absolute zero (-273.15), please try again!"
     return (cels * 9 / 5) + 32
 
 
