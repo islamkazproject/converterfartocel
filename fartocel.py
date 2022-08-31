5d8ba633636b7edd2ba9a921d7fac13d40c433cd
Islamkazproject <39201439+islamkazproject@users.noreply.github.com>
2022-08-31 14:26:50 +0300

    Delete fartocel.py

--- fartocel.py
+++
@@ -1,8 +0,0 @@
-def far_to_cel(far):
-    if type(far) not in [int, float]:
-        raise TypeError("Celsius must be non-negative real number only")
-    return (far - 32) * 5 / 9
-
-
-# if __name__ == "__main__":
-#     print(far_to_cel(-40))
