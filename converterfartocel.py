c620c3ed4af05482b36dfb497972f2847ecb6d63
Islam <islamkazproject@gmail.com>
2022-09-01 15:42:30 +0300

    Delete file

--- converterfartocel.py
+++
@@ -1,8 +0,0 @@
-def cel_to_far(cels):
-    if type(cels) not in [int, float]:
-        raise TypeError("Celsius must be non-negative real number only")
-    return (cels * 9 / 5) + 32
-
-
-# if __name__ == "__main__":
-#     print(cel_to_far(90))
