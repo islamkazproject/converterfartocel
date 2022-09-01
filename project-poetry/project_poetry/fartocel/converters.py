12a17ff0815f03953a8ec2d30cea8878256c6a24
Islam <islamkazproject@gmail.com>
2022-09-01 16:29:27 +0300

    Edit project

---
+++ project-poetry/project_poetry/fartocel/converters.py
@@ -0,0 +1,21 @@
+def cel_to_far(cels: str) -> int or float:
+    """Returns converted Celsius to Fahrenheit."""
+    if type(cels) not in [int, float]:
+        raise TypeError("Celsius must be non-negative real number only")
+    if cels < -273.15:
+        return "Error, the number cannot be lower than absolute zero (-273.15), please try again!"
+    return (cels * 9 / 5) + 32
+
+
+def far_to_cel(far: str) -> int or float:
+    """Returns converted Fahrenheit to Celsius"""
+    if type(far) not in [int, float]:
+        raise TypeError("Fahrenheit must be non-negative real number only")
+    if far < -459.66999999999996:
+        return "Error, the number cannot be lower than absolute zero (-459.67), please try again!"
+    return (far - 32) * 5 / 9
+
+
+if __name__ == "__main__":
+    print(cel_to_far(-273))
+    print(far_to_cel(-459))
--- project-poetry/project_poetry/fartocel/tests/test_converters.py
+++ project-poetry/project_poetry/fartocel/tests/test_converters.py
@@ -1,5 +1,5 @@
 import unittest
-from converterfartocel import cel_to_far
+from converters import cel_to_far
 
 
 class TestCelsToFar(unittest.TestCase):
@@ -12,6 +12,11 @@ class TestCelsToFar(unittest.TestCase):
         self.assertEqual(cel_to_far(-20), -4)
         self.assertEqual(cel_to_far(-273.15), -459.67)
 
+        self.assertEqual(far_to_cel(32), 0.0)
+        self.assertEqual(far_to_cel(5), -15.0)
+        self.assertEqual(far_to_cel(194), 90.0)
+        self.assertEqual(far_to_cel(-40), -40.0)
+
     def test_types(self):
 
         self.assertRaises(TypeError, cel_to_far, 5 + 2j)
@@ -20,3 +25,8 @@ class TestCelsToFar(unittest.TestCase):
         self.assertRaises(TypeError, cel_to_far, True)
         self.assertRaises(TypeError, cel_to_far, [42])
 
+        self.assertRaises(TypeError, far_to_cel, 5 + 2j)
+        self.assertRaises(TypeError, far_to_cel, "five")
+        self.assertRaises(TypeError, far_to_cel, [16, 22])
+        self.assertRaises(TypeError, far_to_cel, True)
+        self.assertRaises(TypeError, far_to_cel, [42])
