7fba9e67850ee9da58200d1b30e88b88153d8883
Islam <islamkazproject@gmail.com>
2022-09-05 12:12:01 +0300

    Changed the word Error to Exception!

--- fartocel/converters.py
+++ fartocel/converters.py
@@ -6,7 +6,7 @@ def cel_to_far(cels: Union[int, float]) -> Union[str, float]:
     if type(cels) not in [int, float]:
         raise TypeError("Celsius must be non-negative real number only")
     if cels < -273.15:
-        return "Error, the number cannot be lower than absolute zero (-273.15), please try again!"
+        return "Exception, the number cannot be lower than absolute zero (-273.15), please try again!"
     return (cels * 9 / 5) + 32
 
 
@@ -15,7 +15,7 @@ def far_to_cel(far: Union[int, float]) -> Union[str, float]:
     if type(far) not in [int, float]:
         raise TypeError("Fahrenheit must be non-negative real number only")
     if far < -459.66999999999996:
-        return "Error, the number cannot be lower than absolute zero (-459.67), please try again!"
+        return "Exception, the number cannot be lower than absolute zero (-459.67), please try again!"
     return (far - 32) * 5 / 9
 
 
